c8ec57a880016f53a8dd36c4e0ac1913a4c44ef0
DavidLucas-01 <davidlucaskl@gmail.com>
2023-07-24 13:22:12 -0300

    Inserção barra de pesquisa na tabela de contatos

--- Contatos/wwwroot/js/site.js
+++ Contatos/wwwroot/js/site.js
@@ -3,7 +3,42 @@
 
 // Write your JavaScript code.
 
+$(document).ready(function (){ 
+    $('#table-contatos').DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado",
+            "sInfo": "Mostrar _START_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Próximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Ultimo"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar Colunas de forma ascendente",
+                "sSortDescending": ": Ordenar Colunas de forma descendente"
+            }
+        }
+        });
+});
+
+
+
 
 $('.close-alert').click(function () {
     $('.alert').hide('hide');
 });
+
+
