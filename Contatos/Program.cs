668ba5315160a7bd67c0fb80d5b221400ab716bb
DavidLucas-01 <davidlucaskl@gmail.com>
2023-07-27 10:41:16 -0300

    Criando tela de login

--- Contatos/Program.cs
+++ Contatos/Program.cs
@@ -33,7 +33,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 
 
